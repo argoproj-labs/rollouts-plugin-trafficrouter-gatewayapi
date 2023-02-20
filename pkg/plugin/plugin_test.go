70d4be5e60b1741db23f77123037487776065ce8
PhilippPlotnikov <philipp.plotnikov@codefresh.io>
2023-02-20 20:35:42 +0400

    Change unit tests
    Signed-off-by: PhilippPlotnikov <philipp.plotnikov@codefresh.io>

--- pkg/plugin/plugin_test.go
+++ pkg/plugin/plugin_test.go
@@ -1,93 +1,101 @@
 package plugin
 
 import (
-	"encoding/json"
+	"context"
 	"testing"
+	"time"
 
-	"github.com/argoproj-labs/rollouts-gatewayapi-trafficrouter-plugin/pkg/plugin/mocks"
 	"github.com/argoproj-labs/rollouts-gatewayapi-trafficrouter-plugin/utils"
-	"github.com/argoproj/argo-rollouts/pkg/apis/rollouts/v1alpha1"
+	rolloutsPlugin "github.com/argoproj/argo-rollouts/rollout/trafficrouting/plugin/rpc"
+
 	log "github.com/sirupsen/logrus"
-	"github.com/tj/assert"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	goPlugin "github.com/hashicorp/go-plugin"
 )
 
-var logCtx = log.WithFields(log.Fields{"plugin": "trafficrouter"})
+var testHandshake = goPlugin.HandshakeConfig{
+	ProtocolVersion:  1,
+	MagicCookieKey:   "ARGO_ROLLOUTS_RPC_PLUGIN",
+	MagicCookieValue: "trafficrouter",
+}
 
-const (
-	stableServiceName = "argo-rollouts-stable-service"
-	canaryServiceName = "argo-rollouts-canary-service"
-	httpRouteName     = "argo-rollouts-http-route"
-)
+func TestRunSuccessfully(t *testing.T) {
+	logCtx := log.WithFields(log.Fields{"plugin": "trafficrouter"})
 
-func init() {
 	utils.SetLogLevel("debug")
 	log.SetFormatter(utils.CreateFormatter("text"))
-}
 
-func TestInitPlugin(t *testing.T) {
-	t.Run("InitPlugin", func(t *testing.T) {
-		t.Parallel()
-
-		// Given
-		rpcPluginImp := &RpcPlugin{
-			LogCtx: logCtx,
-		}
-		rollout := newRollout(stableServiceName, canaryServiceName, httpRouteName)
-
-		// When
-		rpcErr := rpcPluginImp.InitPlugin(rollout)
-
-		// Given
-		assert.Equal(t, "", rpcErr.Error())
-	})
-}
-
-func TestSetWeight(t *testing.T) {
-	t.Run("SetWeight", func(t *testing.T) {
-		t.Parallel()
-
-		// Given
-		rpcPluginImp := &RpcPlugin{
-			LogCtx: logCtx,
-			Client: &mocks.FakeClient{},
-		}
-		rollout := newRollout(stableServiceName, canaryServiceName, httpRouteName)
-
-		// When
-		rpcErr := rpcPluginImp.SetWeight(rollout, 30, []v1alpha1.WeightDestination{})
-
-		// Given
-		assert.Equal(t, "", rpcErr.Error())
-	})
-}
-
-func newRollout(stableSvc, canarySvc, httpRouteName string) *v1alpha1.Rollout {
-	gatewayAPIConfig := GatewayAPITrafficRouting{
-		HTTPRoute: httpRouteName,
-		Namespace: "default",
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	rpcPluginImp := &RpcPlugin{
+		LogCtx: logCtx,
 	}
-	encodedGatewayAPIConfig, err := json.Marshal(gatewayAPIConfig)
+
+	// pluginMap is the map of plugins we can dispense.
+	var pluginMap = map[string]goPlugin.Plugin{
+		"RpcTrafficRouterPlugin": &rolloutsPlugin.RpcTrafficRouterPlugin{Impl: rpcPluginImp},
+	}
+
+	ch := make(chan *goPlugin.ReattachConfig, 1)
+	closeCh := make(chan struct{})
+	go goPlugin.Serve(&goPlugin.ServeConfig{
+		HandshakeConfig: testHandshake,
+		Plugins:         pluginMap,
+		Test: &goPlugin.ServeTestConfig{
+			Context:          ctx,
+			ReattachConfigCh: ch,
+			CloseCh:          closeCh,
+		},
+	})
+
+	// We should get a config
+	var config *goPlugin.ReattachConfig
+	select {
+	case config = <-ch:
+	case <-time.After(2000 * time.Millisecond):
+		t.Fatal("should've received reattach")
+	}
+	if config == nil {
+		t.Fatal("config should not be nil")
+	}
+
+	// Connect!
+	c := goPlugin.NewClient(&goPlugin.ClientConfig{
+		Cmd:             nil,
+		HandshakeConfig: testHandshake,
+		Plugins:         pluginMap,
+		Reattach:        config,
+	})
+	client, err := c.Client()
 	if err != nil {
-		log.Fatal(err)
+		t.Fatalf("err: %s", err)
 	}
-	return &v1alpha1.Rollout{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "rollout",
-			Namespace: "default",
-		},
-		Spec: v1alpha1.RolloutSpec{
-			Strategy: v1alpha1.RolloutStrategy{
-				Canary: &v1alpha1.CanaryStrategy{
-					StableService: stableSvc,
-					CanaryService: canarySvc,
-					TrafficRouting: &v1alpha1.RolloutTrafficRouting{
-						Plugin: map[string]json.RawMessage{
-							"argoproj-labs/gatewayAPI": encodedGatewayAPIConfig,
-						},
-					},
-				},
-			},
-		},
+
+	// Pinging should work
+	if err := client.Ping(); err != nil {
+		t.Fatalf("should not err: %s", err)
 	}
+
+	// Kill which should do nothing
+	c.Kill()
+	if err := client.Ping(); err != nil {
+		t.Fatalf("should not err: %s", err)
+	}
+
+	// Request the plugin
+	raw, err := client.Dispense("RpcTrafficRouterPlugin")
+	if err != nil {
+		t.Fail()
+	}
+
+	pluginInstance := raw.(rolloutsPlugin.RpcTrafficRouterPlugin)
+
+	err = pluginInstance.Impl.InitPlugin()
+	if err != nil {
+		t.Fail()
+	}
+
+	// Canceling should cause an exit
+	cancel()
+	<-closeCh
 }
